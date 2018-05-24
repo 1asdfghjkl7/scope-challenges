133fdbe13c023ce2aa20d9c8c8d65005812e0674
Jordan <jordanwilliams288@gmail.com>
2018-05-24 14:27:54 -0500

    completed scope challenges

---
+++ main.js
@@ -0,0 +1,89 @@
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let x = 1
+
+// for (let x = 1; x < cookies.length; x++) {
+//     const currentCookie = cookies[x]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+
+
+// const conjunction = function (firstWord, secondWord) {
+//     const conjoinedWord = `${firstWord} ${secondWord}`
+//     console.log(conjoinedWord)
+// }
+
+// conjunction("Master", "Card")
+
+
+
+//     const ModSquad = {
+//         'members':["Pete Cochran","Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+//         'series': {'start': "1968", 'end': "1973"}
+//     }
+
+//     let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+//     ModSquad.members.forEach(member => {
+//         HTMLRepresentation += `<div>${member}</div>`
+//     })
+
+
+// document.querySelector(".show-info").innerHTML = HTMLRepresentation
+
+
+
+const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+for (let k = 0; k < locations.length; k++) {
+    const currentLocation = locations[k]
+    let invalidLocation;
+
+    if (currentLocation[0] > 2) {
+        const invalidLocation = true
+    }
+
+    if (invalidLocation) {
+        console.log("This location is invalid")
+    }
+    console.log(`There were ${k} locations displayed`)
+}
+
+
+//OOOOOORRRR
+
+// const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+// for (var k = 0; k < locations.length; k++) {
+//     const currentLocation = locations[k]
+//     let invalidLocation;
+
+//     if (currentLocation[0] > 2) {
+//         const invalidLocation = true
+//     }
+
+//     if (invalidLocation) {
+//         console.log("This location is invalid")
+//     }
+// }
+
+// console.log(`There were ${k} locations displayed`)
+
+
+
+// const llamaNamer = function () {
+//     const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+//     const randomizer = Math.floor(Math.random() * 7)
+
+    
+//         const suffix = " the Llama"
+//         const name = possibleNames[randomizer]
+//         return name + suffix
+    
+
+    
+
+// }
+
+// nameMaker = llamaNamer()
+// console.log(nameMaker)
